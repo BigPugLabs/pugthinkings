6122c9205812044bdb784189c1c31f14b13856e6
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-03-28 02:14:35 +0100

    First line of note used for sidebar label

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,25 @@
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
+    const noteElements = props.notes.map((note, index) => {
 
-                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
-                    }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+        // find the start and end of the first line without formatting characters
+        let startOfFirstLine = note.body.search(/[a-z0-9]/i)
+        let endOfFirstLine = note.body.indexOf("\n")
+        if (startOfFirstLine == -1) startOfFirstLine = note.body.length
+        if (endOfFirstLine == -1) endOfFirstLine = note.body.length
+
+        return (
+            <div key={note.id}>
+                <div
+
+                    className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
+                        }`}
+                    onClick={() => props.setCurrentNoteId(note.id)}
+                >
+                    <h4 className="text-snippet">{note.body.slice(startOfFirstLine, endOfFirstLine) || "Note " + (index + 1)}</h4>
+                </div>
             </div>
-        </div>
-    ))
+        )
+    })
 
     return (
         <section className="pane sidebar">
